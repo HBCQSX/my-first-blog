81cd6603f4aebd8aa880a3b339742edd67041478
admin <2441189986@qq.com>
2025-04-12 13:49:52 +0800

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
+from django.urls import re_path as url
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 ]
